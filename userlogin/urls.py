90b21fbec04a676ea9c422e978574336aa44c6a1
Rayyan Merchant <rayyanmerchant7@gmail.com>
2020-02-05 14:31:19 +0530

    added formone

--- userlogin/urls.py
+++ userlogin/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('register/', views.RegisterView, name='register'),
     path('login/',auth_views.LoginView.as_view(template_name='userlogin/login.html'), name='login'),
     path('logout/',auth_views.LogoutView.as_view(template_name='userlogin/logout.html'), name='logout'),
+    path('formone/', views.FormOneView, name='form_one_page'),
+    path('accepted/', views.AcceptedView, name='accepted_page'),
+    path('rejected/', views.RejectedView, name='rejected_page'),
 ]
--- userlogin/views.py
+++ userlogin/views.py
@@ -2,8 +2,42 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
+from .forms import FormOne
 
 
+def FormOneView(request):
+    formone = FormOne()
+    if request.method == 'POST':
+        formone = FormOne(request.POST)
+        
+        if formone.is_valid():
+            email = formone.cleaned_data.get('email')
+            name = formone.cleaned_data.get('name')
+            phone_number = formone.cleaned_data.get('phone_number')
+            clubs = formone.cleaned_data.get('clubs')
+            city = formone.cleaned_data.get('city')
+            print("clubs = ", clubs)
+            print("city = ", city)
+            print(type(city))
+            if clubs == 'Other' or city == 'Other':
+                return render(request, 'userlogin/rejected.html')
+                # redirect('/rejected')
+            else :
+                return render(request, 'userlogin/accepted.html')
+                # redirect('/accepted')
+    else:
+        formone = FormOne()
+    
+    context = {
+        'form' : formone
+    }
+    return render(request, 'userlogin/formone.html', context)
+
+def RejectedView(request):
+    return render(request, 'userlogin/accepted.html')
+
+def AcceptedView(request):
+    return render(requets, 'userlogin/rejected.html')
 
 
 def index(request):
