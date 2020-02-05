4340986db6c232fbf71f4fa0b270d4912eb0677c
sauravjoshi23 <sauravjoshi2362000@gmail.com>
2020-02-05 10:56:43 +0530

    second commit

--- userlogin/views.py
+++ userlogin/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-# Create your views here.
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+
+
 
 def index(request):
     return HttpResponse('works')
+
+def RegisterView(request):
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+        form.save()
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=password)
+        login(request, user)
+        return redirect('index_page')
+    return render(request, 'userlogin/register.html', {'form': form})
